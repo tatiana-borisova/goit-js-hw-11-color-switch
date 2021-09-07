6c9066e5719a9c581498f1fec334b4aaaf2dae45
Tatiana Borisova <81636916+tatiana-borisova@users.noreply.github.com>
2021-09-07 21:36:41 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+//variables
 const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+const refs = {
+  bodyEl: document.querySelector('body'),
+  btnStartEl: document.querySelector('[data-action="start"]'),
+  btnStopEl: document.querySelector('[data-action="stop"]'),
+};
+const { bodyEl, btnStartEl, btnStopEl } = refs;
+let timerId = null;
+
+//functions
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//event listeners
+btnStartEl.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+  btnStartEl.disabled = true;
+});
+btnStopEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStartEl.disabled = false;
+});
